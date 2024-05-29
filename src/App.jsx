5896aedd73279b66b02a2cfa43afce3e6575ccec
alidu28 <mugabejunior5@gmail.com>
2024-05-29 01:31:57 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React from "react"
+import Navbar from "./components/Navbar/Navbar"
+import Hero from "./components/Hero/Hero"
+
+function App() {
+
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default App
