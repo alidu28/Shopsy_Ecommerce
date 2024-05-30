cfaaef90964f13136ecc00bd73046e321f264ac0
alidu28 <mugabejunior5@gmail.com>
2024-05-30 23:51:45 +0000

    "Updated"

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import React from "react"
 import Navbar from "./components/Navbar/Navbar"
 import Hero from "./components/Hero/Hero"
+import Category from "./components/Category/Category"
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Navbar/>
       <Hero/>
+      <Category/>
     </div>
   )
 }
